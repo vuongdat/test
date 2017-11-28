44c5a222f77ce7db7e77942510053b7bc64abab4
vuongdat <dat.vt@kayac.vn>
2017-11-28 08:32:44 +0700

    update project

---
+++ T1/Assets/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    //Object camera will follow to followTarget object
+    [SerializeField]
+    private GameObject followTarget;
+    //How far camera stays from target
+    private Vector3 followDistance;
+
+    /// <summary>
+    /// Start this instance.
+    /// </summary>
+    private void Start ()
+    {
+        followDistance = transform.position - followTarget.transform.position;
+    }
+
+    /// <summary>
+    /// Update this instance.
+    /// </summary>
+    private void Update ()
+    {
+        transform.position = followTarget.transform.position + followDistance;
+    }
+}
